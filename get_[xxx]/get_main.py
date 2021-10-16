11c9294633a31edf7b8ef637504f4e69b7c5a771
liutq <1355583269@qq.com>
2021-10-16 14:47:45 +0800

    update

--- get_[xxx]/get_main.py
+++ get_[xxx]/get_main.py
@@ -1,26 +1,19 @@
 import re
 
 f = open("file.md","rt",encoding='utf-8')
-F = open("new_file.txt","w",encoding='utf-8')
+F = open("new_file.md ","w",encoding='utf-8')
 lines=f.readlines()
 
 for i in lines:
     text=i
     pattern=re.compile("\\[(.*?)\\]\\((.*?)\\)")
     content=pattern.findall(text)
-    if content != []:
-        # print(content)
-        for j in content:        
-            new_content=j[0]
-            # print(new_content)
-            text=pattern.sub(new_content,text)
-            F.write(text)
+    if len(content) != 0:
+        for tuple in content:
+            title =  tuple[0]
+            text = text.replace("[{}]({})".format(tuple[0], tuple[1]), tuple[0])
+        F.write(text)
     else:
         F.write(text)
-f.close
-F.close
-
-
-
-
-    
+f.close()
+F.close()
