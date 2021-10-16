c3b2d31cfa5d9b9d14ee9ffd138ecbee1ca03066
liutq <1355583269@qq.com>
2021-10-16 14:49:46 +0800

    update

--- get_[xxx]/get_main.py
+++ get_[xxx]/get_main.py
@@ -4,13 +4,11 @@ f = open("file.md","rt",encoding='utf-8')
 F = open("new_file.md ","w",encoding='utf-8')
 lines=f.readlines()
 
-for i in lines:
-    text=i
+for text in lines:
     pattern=re.compile("\\[(.*?)\\]\\((.*?)\\)")
     content=pattern.findall(text)
     if len(content) != 0:
         for tuple in content:
-            title =  tuple[0]
             text = text.replace("[{}]({})".format(tuple[0], tuple[1]), tuple[0])
         F.write(text)
     else:
